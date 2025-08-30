5c35ab2bc55cedae973aa83b111ea001dc7b56e2
Vitaliy Shagin <redwind090@gmail.com>
2025-08-30 08:50:13 +0300

    Убран синоним в импорте

--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"my_documents_south_backend/internal/core/facade"
-	postgres2 "my_documents_south_backend/internal/storage/postgres"
+	"my_documents_south_backend/internal/storage/postgres"
 	mdsHttp "my_documents_south_backend/internal/transport/http"
 
 	"github.com/gofiber/fiber/v2"
@@ -13,15 +13,15 @@ func main() {
 		Immutable: true,
 	})
 
-	db := postgres2.New()
+	db := postgres.New()
 	defer db.Close()
 
 	repository := facade.NewRepository(db)
 
 	services := facade.NewService(repository)
 
-	http_handler := mdsHttp.NewHttpHander(services)
-	http_handler.Route(app)
+	httpHandler := mdsHttp.NewHttpHander(services)
+	httpHandler.Route(app)
 
 	if err := app.Listen(":3000"); err != nil {
 		panic(err)
